4b865f7ddc82d7d965f2c9bee891d90669aaed1c
sgao@beckman.com <sgao@beckman.com>
2020-09-23 09:42:46 +0800

    Deprecated HttpServletResponseWrapper

--- douyaba-framework-web-protocol/douyaba-framework-web-protocol-model/src/main/java/com/github/douyaba/web/protocol/model/HttpServletResponseWrapper.java
+++ douyaba-framework-web-protocol/douyaba-framework-web-protocol-model/src/main/java/com/github/douyaba/web/protocol/model/HttpServletResponseWrapper.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 /**
  * @author s.c.gao
  */
+@Deprecated
 public class HttpServletResponseWrapper extends javax.servlet.http.HttpServletResponseWrapper {
 
     private final ByteArrayOutputStream bos = new ByteArrayOutputStream();
